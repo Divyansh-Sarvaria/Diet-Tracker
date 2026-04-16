44f5b7c37c61e4321939067d38fc3ebebc5ebb92
Divyansh-Sarvaria <divyanshsarvaria@gmail.com>
2026-04-16 16:04:52 +0530

    cors

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -9,7 +9,12 @@ const app = express();
 
 connectDb();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  }),
+);
 app.use(express.json());
 
 app.use("/auth", authRoutes);
